df5d3faba4dabfdb6fbd398b6919d5ee83d5063b
AvetisHarutyunyan <avetis.harutyunyan.98@gmail.com>
2022-03-12 18:50:52 +0400

    update JS and CSS

--- js/app.js
+++ js/app.js
@@ -6,23 +6,35 @@ function navSide() {
     const closeBtn = document.querySelector("#closeBtn");
     const closeNavWithBody = document.querySelector("body");
 
-    burger.addEventListener("click", function() {
+    burger.addEventListener("click", function () {
         menuNav.classList.add("nav-active");
 
         // body to left
         closeNavWithBody.classList.add("left");
     });
 
-    closeBtn.addEventListener("click", function() {
+    closeBtn.addEventListener("click", function () {
         menuNav.classList.remove("nav-active");
         closeNavWithBody.classList.remove("left");
     });
 
-    closeNavWithBody.addEventListener("mouseup", function(e) {
-        if(e.target !== closeBtn && e.target !== menuNav) {
+    closeNavWithBody.addEventListener("mouseup", function (e) {
+        if (e.target !== closeBtn && e.target !== menuNav) {
             menuNav.classList.remove("nav-active");
             closeNavWithBody.classList.remove("left");
         }
     });
 }
 navSide();
+
+// Selected some house
+function homeSelected() {
+    const heartIcon = document.querySelectorAll(".heart_icon");
+
+    heartIcon.forEach(item => {
+        item.addEventListener("click", function() {
+            item.classList.toggle("heart-active");
+        });
+    });
+}
+homeSelected();
